291b290d39fff801d3b05516bb3e975239684c54
msps50807 <msps50807@gmail.com>
2019-04-07 10:15:52 -0700

    first fail test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,29 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+	# Setup anything we need before the test.
+	def setUp(self):
+		# Get a webdriver of Firefox.
+		self.browser = webdriver.Firefox()
+		# Ask selenium wait 3 three seconds to do anything.
+		self.browser.implicitly_wait(3)
+	
+	# After the test, clean anything we don't need anymore.
+	def tearDown(self):
+		# Close the webdriver.
+		self.browser.quit()
+	
+	# First testcase we need. It should start with 'test-' and the testcase.
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Open the page.
+		self.browser.get('http://localhost:8000')
+
+		# Check the browser's title.
+		self.assertIn('To-Do', self.browser.title)
+		# It will fail anyway. We use it to finish the test.
+		self.fail('Finish the test!')
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
