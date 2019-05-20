6d9aeefb3727453df6f05d9d2e461fb81a566f58
msps50807 <msps50807@gmail.com>
2019-05-20 00:13:58 -0700

    make functional_tests ba app and use LiveServerTestCase

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 import unittest
 import time
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	# Setup anything we need before the test.
 	def setUp(self):
@@ -16,6 +17,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		# Close the webdriver.
 		self.browser.quit()
+
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
 	
 	# First testcase we need. It should start with 'test-' and the testcase.
 	def test_can_start_a_list_and_retrieve_it_later(self):
@@ -24,27 +30,30 @@ class NewVisitorTest(unittest.TestCase):
 
 		# Check the browser's title.
 		self.assertIn('To-Do', self.browser.title)
-		
+
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
+
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
 		)
 
 		inputbox.send_keys('Buy peacock feathers')
-		
 		inputbox.send_keys(Keys.ENTER)
-		#Use time.sleep to debug.
+
 		time.sleep(1)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row  in rows])
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 
-		self.fail('Finish the test')
+		time.sleep(1)
 
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+		self.fail('Finish the test!')
+
